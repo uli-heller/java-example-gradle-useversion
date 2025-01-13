4e07995a2ae9a91430f7c465e032294c525acf03
Uli Heller <uli.heller@daemons-point.com>
2025-01-13 21:26:24 +0100

    Create multiple versions for maven repo

--- create-maven-repository.sh
+++ create-maven-repository.sh
@@ -5,5 +5,7 @@ D="$(realpath "${D}")"
 MAVEN_REPOSITORY="${D}/maven-repository"
 MAVEN_REPOSITORY_URL="file://${MAVEN_REPOSITORY}"
 
-( cd hello-world; "${D}/gradlew" publish -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" )
-( cd bye-moon; "${D}/gradlew" publish -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" )
+( cd hello-world; "${D}/gradlew" publish -Pversion=0.0.0 -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" )
+( cd hello-world; "${D}/gradlew" publish -Pversion=0.1.0 -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" )
+( cd bye-moon; "${D}/gradlew" publish -Pversion=0.0.0 -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" )
+( cd bye-moon; "${D}/gradlew" publish -Pversion=0.0.1 -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" )
