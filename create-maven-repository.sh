a542a06938f2fdc25ca9699c3ae0841417c8c593
Uli Heller <uli.heller@daemons-point.com>
2025-01-15 07:44:16 +0100

    create-maven-repository: version-start and version-count

--- create-maven-repository.sh
+++ create-maven-repository.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# ./create-maven-repository.sh (version-cnt)
+# ./create-maven-repository.sh (version-start) (version-cnt)
 #
 D="$(dirname "$0")"
 D="$(realpath "${D}")"
@@ -10,11 +10,14 @@ MAVEN_REPOSITORY_URL="file://${MAVEN_REPOSITORY}"
 
 rm -rf "${MAVEN_REPOSITORY}"
 
-CNT="$1"
+VERSION_START="$1"
+test -z "${VERSION_START}" && VERSION_START=0.
+
+CNT="$2"
 test -z "$CNT" && CNT=5
 
 for V in $(seq 1 ${CNT}); do
-  VERSION="0.${V}.0"
+  VERSION="${VERSION_START}.${V}.0"
   ( cd hello-world; "${D}/gradlew" publish -Pversion=${VERSION} -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" )
   ( cd bye-moon; "${D}/gradlew" publish -Pversion=${VERSION} -PmavenRepositoryUrl="${MAVEN_REPOSITORY_URL}" )
 done
